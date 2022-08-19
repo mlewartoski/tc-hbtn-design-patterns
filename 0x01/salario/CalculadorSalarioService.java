c152c91f282bbdc436ca5cf33c4ef296a153b31c
mlewartoski <mlewartoski@ciandt.com>
2022-08-19 09:32:30 -0300

    padrões de projeto 2, task 0 #0

---
+++ 0x01/salario/CalculadorSalarioService.java
@@ -0,0 +1,23 @@
+package salario;
+
+import java.util.UUID;
+
+public class CalculadorSalarioService {
+    public UUID uuid = UUID.randomUUID();
+    static CalculadorSalarioService instancia;
+
+    private CalculadorSalarioService(){
+
+    }
+
+    public static CalculadorSalarioService getCalculadorSalarioService(){
+        if (instancia == null) {
+            instancia = new CalculadorSalarioService();
+        }
+        return instancia;
+    }
+
+    public double calcularSalarioLiquido(double salarioBruto, double valorDescontos, double valorVendas, double percentualComissao){
+        return salarioBruto - valorDescontos + (valorVendas * (percentualComissao / 100));
+    }
+}
---
+++ 0x01/salario/SalarioService.java
@@ -0,0 +1,15 @@
+package salario;
+
+import java.util.UUID;
+
+public class SalarioService {
+    private CalculadorSalarioService calculador = CalculadorSalarioService.getCalculadorSalarioService();
+
+    public double calcular(double salarioBruto, double valorDescontos, double valorVendas, double percentualComissao){
+        return this.calculador.calcularSalarioLiquido(salarioBruto, valorDescontos, valorVendas, percentualComissao);
+    }
+
+    public UUID getUuid(){
+        return calculador.uuid;
+    }
+}
